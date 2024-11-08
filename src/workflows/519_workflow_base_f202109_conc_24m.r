526d33aa2122e6926422855a238f98da8fcd8f98
RobertoInza <robertoinza@gmail.com>
2024-11-08 18:39:06 +0000

    auto commit 27       20241108 183858

--- src/workflows/519_workflow_base_f202109_conc_24m.r
+++ src/workflows/519_workflow_base_f202109_conc_24m.r
@@ -277,12 +277,16 @@ TS_strategy_base9 <- function( pinputexps )
 
   param_local$final_train$undersampling <- 1.0
   param_local$final_train$clase_minoritaria <- c( "BAJA+1", "BAJA+2")
-  param_local$final_train$training <- c(202107, 202106, 202105, 202104, 202103, 202102,
-    202101, 202012, 202011)
+  param_local$final_train$training <- c(202107,	202106,	202105,	202104,	202103,	202102,
+                                        202101,	202012,	202011,	202010,	202009,	202008,
+                                        202007,	202006,	202005,	202004,	202003,	202002,
+                                        202001,	201912,	201911,	201910,	201909,	201908)
 
 
-  param_local$train$training <- c(202105, 202104, 202103, 202102, 202101,
-    202012, 202011, 202010, 202009)
+  param_local$train$training <- c(202105,	202104,	202103,	202102,	202101,	202012,	202011,	
+                                  202010,	202009,	202008,	202007,	202006,	202005,	202004,
+                                  202003,	202002,	202001,	201912,	201911,	201910,	201909,	
+                                  201908,	201907,	201906)
   param_local$train$validation <- c(202106)
   param_local$train$testing <- c(202107)
 
@@ -427,7 +431,7 @@ KA_evaluate_kaggle <- function( pinputexps )
 # Que predice 202107 donde conozco la clase
 # y ya genera graficos
 
-wf_septiembre <- function( pnombrewf )
+wf_24m <- function( pnombrewf )
 {
   param_local <- exp_wf_init( pnombrewf ) # linea fija
 
@@ -458,5 +462,5 @@ wf_septiembre <- function( pnombrewf )
 # Aqui comienza el programa
 
 # llamo al workflow con future = 202109
-wf_septiembre()
+wf_24m()
 
