e55f8f31c1fd723f865d7e35d2360ffee03d5792
RobertoInza <robertoinza@gmail.com>
2024-11-09 14:05:12 +0000

    auto commit 28       20241109 140504

--- src/workflows/501_run_workflow_conceptual_12m_cc.r
+++ src/workflows/501_run_workflow_conceptual_12m_cc.r
@@ -1,7 +1,7 @@
 require("rlang")
 
 # workflow que voy a correr
-PARAM <- "src/workflows/517_workflow_base_f202107_conc_9m.r"
+PARAM <- "src/workflows/517_workflow_base_f202107_conc_12m.r"
 
 envg <- env()
 
